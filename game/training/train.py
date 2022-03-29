77c4303a0f5c0a2879aede90b29ef7ad27b0b679
Maxime Charpentier <pro.maxime.charpentier@gmail.com>
2022-03-29 09:00:27 +0200

    isort

--- game/training/train.py
+++ game/training/train.py
@@ -1,8 +1,8 @@
 from copy import deepcopy
 from typing import List, Tuple
-from torch import multiprocessing
 
 import numpy as np
+from torch import multiprocessing
 from tqdm.notebook import tqdm
 
 import constants
@@ -75,42 +75,6 @@ def evaluate_agent(
             win_rates[i].append(win_rate_vs_opponent(agent, opponent, env, n_test_runs))
         else:
             win_rates[i].append(win_rate_vs_self(agent, opponent, env, n_test_runs))
-
-
-def plot_win_rates(
-    win_rates: List[List[float]],
-    losses: List[float],
-    interval_test: int,
-    path="progress.png",
-):
-    fig, ax = plt.subplots(1, 2, figsize=(15, 8))
-
-    fig.patch.set_facecolor("#f2f2f2")
-    num_tests = len(win_rates[0])
-    for i, win_rate in enumerate(win_rates):
-        if len(win_rate) <= 1:
-            continue
-
-        start = num_tests - len(win_rate)
-        if i == 0:
-            label = "Random Agent"
-        else:
-            label = f"Opponent trained {start * interval_test} episode"
-        ax[0].plot(
-            [i * interval_test for i in range(start, num_tests)], win_rate, label=label
-        )
-    ax[0].set_title("Win rate against opponent at different stages of training")
-    ax[0].set_xlabel(f"Episodes")
-    ax[0].set_ylim(0, 1)
-    ax[0].legend()
-
-    pd.DataFrame(losses).rolling(500).mean().plot(ax=ax[1])
-    ax[1].set_title("Loss")
-    ax[1].set_xlabel(f"Episodes")
-    ax[1].get_legend().remove()
-
-    fig.savefig(path)
-    plt.close(fig)
 
 
 def train_against_self(
